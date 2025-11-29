1b1dcda4a82c887a3d8f53956e84dfebbcc77d9c
Jorge Pereira <55901804+jorgep23@users.noreply.github.com>
2025-11-29 01:58:31 -0300

    Update price.js

--- commands/price.js
+++ commands/price.js
@@ -1,9 +1,11 @@
 const { bot } = require("../index");
-const { getPrice } = require("../services/priceService");
+const { getPrice } = require("../services/price");
 
 bot.onText(/\/price/, async (msg) => {
   const chatId = msg.chat.id;
+
   let price = await getPrice();
   price = Number(price) || 0;
-  bot.sendMessage(chatId, `💰 Preço HBR: ${price.toFixed(8)} USD`);
+
+  bot.sendMessage(chatId, `💰 Preço HBR: $${price.toFixed(8)} USD`);
 });
